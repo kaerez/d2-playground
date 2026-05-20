52cd2af5c23c4a8be7ce5ca8c1e5d0601b6556fb
kaerez <kaerez@users.noreply.github.com>
2026-05-20 18:44:17 +0300

    Update vite.config.ts

--- src/js/vite.config.ts
+++ src/js/vite.config.ts
@@ -1,19 +1,21 @@
 import { defineConfig } from "vite";
 import monacoEditorPlugin from "vite-plugin-monaco-editor";
+import { resolve } from "path";
 
 export default defineConfig({
+  root: resolve(__dirname, "../"), // Maps the root directory up out of js/ to locate index.html cleanly
+  base: "./",
   plugins: [
     monacoEditorPlugin({
       languages: ["json"]
     })
   ],
   build: {
-    outDir: "dist",
+    outDir: resolve(__dirname, "dist"), // Outputs compilation files directly to /src/js/dist
     emptyOutDir: true,
     chunkSizeWarningLimit: 2000,
     rollupOptions: {
       output: {
-        // Enforce deterministic, un-hashed module asset filenames
         entryFileNames: "assets/[name].js",
         chunkFileNames: "assets/[name].js",
         assetFileNames: "assets/[name].[ext]"
