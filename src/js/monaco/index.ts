ae9f055d2a5276c9b3d1ebac1af4cae2614b97d6
kaerez <kaerez@users.noreply.github.com>
2026-05-20 18:18:21 +0300

    Update index.ts

--- src/js/monaco/index.ts
+++ src/js/monaco/index.ts
@@ -1,9 +1,6 @@
 import * as monaco from "monaco-editor";
 import { createOnigScanner, createOnigString, loadWASM } from "vscode-oniguruma";
 
-// VITE WEB WORKER INJECTION: Load the raw worker entry script inline via Vite syntax
-import EditorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
-
 // Most of this code is derived from https://github.com/bolinfest/monaco-tm
 import { rehydrateRegexps } from "./configuration";
 import { registerLanguages } from "./register";
@@ -11,13 +8,6 @@ import { SimpleLanguageInfoProvider, ScopeNameInfo, TextMateGrammar } from "./pr
 
 import * as config from "./language-configuration.json";
 import d2Grammar from "./d2.tmLanguage.json";
-
-// Establish Monaco worker allocation map globally on self before construction
-(self as any).MonacoEnvironment = {
-  getWorker: function (_workerId: string, _label: string) {
-    return new EditorWorker();
-  }
-};
 
 const languages: monaco.languages.ILanguageExtensionPoint[] = [
   {
@@ -53,18 +43,12 @@ const fetchConfiguration = async (): Promise<monaco.languages.LanguageConfigurat
   return config as monaco.languages.LanguageConfiguration;
 };
 
-// Derived from https://github.com/microsoft/vscode/blob/829230a5a83768a3494ebbc61144e7cde9105c73/src/vs/workbench/services/textMate/browser/textMateService.ts#L33-L40
 async function loadVSCodeOnigurumWASM(): Promise<Response | ArrayBuffer> {
-  // PUBLIC ROOT REFACTOR: Point absolute domain route directly to your public folder asset
   const response = await fetch("/onig.wasm");
   const contentType = response.headers.get("content-type");
   if (contentType === "application/wasm") {
     return response;
   }
-
-  // Using the response directly only works if the server sets the MIME type 'application/wasm'.
-  // Otherwise, a TypeError is thrown when using the streaming compiler.
-  // We therefore use the non-streaming compiler fallback.
   return response.arrayBuffer();
 }
 
